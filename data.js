785a73f3c94d4467e7efcd954aa7a0f89b73421a
Abdulaziz <91874905+AbdulazizFE@users.noreply.github.com>
2022-04-24 18:08:18 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,763 @@
+let songs = [
+  {
+    name: "الفاتحة",
+    path: "all/mp3/1.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "البقرة",
+    path: "all/mp3/2.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "آل عمران",
+    path: "all/mp3/3.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "النساء",
+    path: "all/mp3/4.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المائدة",
+    path: "all/mp3/5.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الأنعام",
+    path: "all/mp3/6.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الأعراف",
+    path: "all/mp3/7.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الأنفال",
+    path: "all/mp3/8.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "التوبة",
+    path: "all/mp3/9.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "يونس",
+    path: "all/mp3/10.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "هود",
+    path: "all/mp3/11.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "يوسف",
+    path: "all/mp3/12.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الرعد",
+    path: "all/mp3/13.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "إبراهيم",
+    path: "all/mp3/14.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الحجر",
+    path: "all/mp3/15.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "النحل",
+    path: "all/mp3/16.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الإسراء",
+    path: "all/mp3/17.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الكهف",
+    path: "all/mp3/18.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "مريم",
+    path: "all/mp3/19.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "طه",
+    path: "all/mp3/20.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الأنبياء",
+    path: "all/mp3/21.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الحج",
+    path: "all/mp3/22.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المؤمنون",
+    path: "all/mp3/23.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "النور",
+    path: "all/mp3/24.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الفرقان",
+    path: "all/mp3/25.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الشعراء",
+    path: "all/mp3/26.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "النمل",
+    path: "all/mp3/27.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "القصص",
+    path: "all/mp3/28.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "العنكبوت",
+    path: "all/mp3/29.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الروم",
+    path: "all/mp3/30.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "لقمان",
+    path: "all/mp3/31.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "السجدة",
+    path: "all/mp3/32.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الأحزاب",
+    path: "all/mp3/33.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "سبأ",
+    path: "all/mp3/34.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "فاطر",
+    path: "all/mp3/35.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "يس",
+    path: "all/mp3/36.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الصافات",
+    path: "all/mp3/37.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "ص",
+    path: "all/mp3/38.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الزمر",
+    path: "all/mp3/39.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "غافر",
+    path: "all/mp3/40.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "فصلت",
+    path: "all/mp3/41.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الشورى",
+    path: "all/mp3/42.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الزخرف",
+    path: "all/mp3/43.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الدّخان",
+    path: "all/mp3/44.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الجاثية",
+    path: "all/mp3/45.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الأحقاف",
+    path: "all/mp3/46.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "محمد",
+    path: "all/mp3/47.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الفتح",
+    path: "all/mp3/48.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الحجرات",
+    path: "all/mp3/49.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "ق",
+    path: "all/mp3/50.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الذاريات",
+    path: "all/mp3/51.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الطور",
+    path: "all/mp3/52.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "النجم",
+    path: "all/mp3/53.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "القمر",
+    path: "all/mp3/54.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الرحمن",
+    path: "all/mp3/55.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الواقعة",
+    path: "all/mp3/56.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الحديد",
+    path: "all/mp3/57.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "المجادلة",
+    path: "all/mp3/58.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الحشر",
+    path: "all/mp3/59.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الممتحنة",
+    path: "all/mp3/60.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الصف",
+    path: "all/mp3/61.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الجمعة",
+    path: "all/mp3/62.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "المنافقون",
+    path: "all/mp3/63.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "التغابن",
+    path: "all/mp3/64.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الطلاق",
+    path: "all/mp3/65.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "التحريم",
+    path: "all/mp3/66.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الملك",
+    path: "all/mp3/67.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "القلم",
+    path: "all/mp3/68.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الحاقة",
+    path: "all/mp3/69.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المعارج",
+    path: "all/mp3/70.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "نوح",
+    path: "all/mp3/71.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الجن",
+    path: "all/mp3/72.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المزمل",
+    path: "all/mp3/73.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المدثر",
+    path: "all/mp3/74.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "القيامة",
+    path: "all/mp3/75.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الإنسان",
+    path: "all/mp3/76.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المرسلات",
+    path: "all/mp3/77.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "النبأ",
+    path: "all/mp3/78.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "النازعات",
+    path: "all/mp3/79.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "عبس",
+    path: "all/mp3/80.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "التكوير",
+    path: "all/mp3/81.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الإنفطار",
+    path: "all/mp3/82.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المطففين",
+    path: "all/mp3/83.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الإنشقاق",
+    path: "all/mp3/84.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "البروج",
+    path: "all/mp3/85.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الطارق",
+    path: "all/mp3/86.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الأعلى",
+    path: "all/mp3/87.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الغاشية",
+    path: "all/mp388.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الفجر",
+    path: "all/mp3/89.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "البلد",
+    path: "all/mp3/90.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الشمس",
+    path: "all/mp3/91.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الليل",
+    path: "all/mp3/92.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الضحى",
+    path: "all/mp3/93.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الشرح",
+    path: "all/mp3/94.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "التين",
+    path: "all/mp3/95.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "العلق",
+    path: "all/mp3/96.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "القدر",
+    path: "all/mp3/97.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "البينة",
+    path: "all/mp3/98.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الزلزلة",
+    path: "all/mp3/99.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "العاديات",
+    path: "all/mp3/100.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "القارعة",
+    path: "all/mp3/101.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "التكاثر",
+    path: "all/mp3/102.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "العصر",
+    path: "all/mp3/103.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الهمزة",
+    path: "all/mp3/104.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الفيل",
+    path: "all/mp3/105.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "قريش",
+    path: "all/mp3/106.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الماعون",
+    path: "all/mp3/107.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الكوثر",
+    path: "all/mp3/108.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الكافرون",
+    path: "all/mp3/109.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "النصر",
+    path: "all/mp3/110.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "المسد",
+    path: "all/mp3/111.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+
+  {
+    name: "الإخلاص",
+    path: "all/mp3/112.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الفلق",
+    path: "all/mp3/113.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+  {
+    name: "الناس",
+    path: "all/mp3/114.mp3",
+    artist: "العفاسي",
+    cover: "all/images/a.jpeg",
+  },
+];
